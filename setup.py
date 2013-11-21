077113f5df326c7dbf04f40fa7d7824c58882b96
James Robert <jiaaro@gmail.com>
2013-11-20 19:55:04 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='daffodil',
+    version='0.0.1',
+    author='James Robert',
+    description='A Super-simple DSL for filtering datasets',
+    license='MIT',
+    keywords='data filtering',
+    url='https://github.com/mediapredict/daffodil',
+    packages=['daffodil'],
+    long_description='A Super-simple DSL for filtering datasets',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Topic :: Utilities'
+    ]
+)
