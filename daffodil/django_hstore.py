d044ffd7212d6b05b93e41e040e8270512e2a3db
James Robert <jiaaro@gmail.com>
2014-06-20 16:28:19 -0400

    Q was imported from the wrong place

--- daffodil/django_hstore.py
+++ daffodil/django_hstore.py
@@ -1,28 +1,26 @@
 
 
 class HStoreQueryDelegate(object):
+    
     def __init__(self, hstore_field_name):
         self.field = hstore_field_name
 
     def mk_any(self, children):
-        from django.db import Q
-
+        from django.db.models import Q
         q = Q()
         for child_q in children:
             q |= child_q
         return q
 
     def mk_all(self, children):
-        from django.db import Q
-
+        from django.db.models import Q
         q = Q()
         for child_q in children:
             q &= child_q
         return q
 
     def mk_test(self, test_str):
-        from django.db import Q
-
+        from django.db.models import Q
         def mk_Q(cmp, hstore_val, negate=False):
             k = "{0}__{1}".format(self.field, cmp)
             q = Q(**{k: hstore_val})
