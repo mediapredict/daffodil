d8706f6a277a70d2fed3640c9869ca94eefdec83
James Robert <jiaaro@gmail.com>
2014-06-20 13:43:13 -0400

    should not require django for predicate support

--- daffodil/django_hstore.py
+++ daffodil/django_hstore.py
@@ -1,4 +1,3 @@
-from django.db import Q
 
 
 class HStoreQueryDelegate(object):
@@ -6,18 +5,24 @@ class HStoreQueryDelegate(object):
         self.field = hstore_field_name
 
     def mk_any(self, children):
+        from django.db import Q
+
         q = Q()
         for child_q in children:
             q |= child_q
         return q
 
     def mk_all(self, children):
+        from django.db import Q
+
         q = Q()
         for child_q in children:
             q &= child_q
         return q
 
     def mk_test(self, test_str):
+        from django.db import Q
+        
         def mk_Q(cmp, hstore_val, negate=False):
             k = "{0}__{1}".format(self.field, cmp)
             q = Q(**{k: hstore_val})
