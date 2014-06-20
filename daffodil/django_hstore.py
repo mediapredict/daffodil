c32bebd2455262de2804ba8356c263243766369f
James Robert <jiaaro@gmail.com>
2014-06-20 13:16:11 -0400

    add any and all support to hstore

--- daffodil/django_hstore.py
+++ daffodil/django_hstore.py
@@ -1,12 +1,18 @@
-
+from django.db import Q
 
 
 class HStoreQueryDelegate(object):
     def mk_any(self, children):
-        return
+    	q = Q()
+    	for child_q in children:
+    		q |= child_q
+        return q
 
     def mk_all(self, children):
-        return
+        q = Q()
+    	for child_q in children:
+    		q &= child_q
+        return q
 
     def mk_test(self, test_str):
         return
