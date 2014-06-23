c76fbaa743a5b98042f314c7a7ee4c0bcfda55ac
Igorkramaric <igor.kramaric@gmail.com>
2014-06-23 22:47:18 +0200

    basic tests working fine

--- daffodil/django_hstore.py
+++ daffodil/django_hstore.py
@@ -1,55 +1,35 @@
-
-
 class HStoreQueryDelegate(object):
     
     def __init__(self, hstore_field_name):
         self.field = hstore_field_name
 
     def mk_any(self, children):
-        from django.db.models import Q
-        q = Q()
-        for child_q in children:
-            q |= child_q
-        return q
+        return " OR ".join( "(" + child_exp + ")" for child_exp in children)
 
     def mk_all(self, children):
-        from django.db.models import Q
-        q = Q()
-        for child_q in children:
-            q &= child_q
-        return q
+        return " AND ".join( "(" + child_exp + ")" for child_exp in children if child_exp )
 
     def mk_test(self, test_str):
-        from django.db.models import Q
-        def mk_Q(cmp, hstore_val, negate=False):
-            k = "{0}__{1}".format(self.field, cmp)
-            q = Q(**{k: hstore_val})
-            return q
-
-        def existance(k, v):
-            q = mk_Q("contains", [k])
-            if not v:
-                q = ~q
-            return q
-
-        ops = {
-          '=':  lambda k, v: mk_Q("contains", {k: v}),
-          '!=': lambda k, v: ~mk_Q("contains", {k: v}),
-          '<':  lambda k, v: mk_Q("lt", {k: v}),
-          '<=': lambda k, v: mk_Q("lte", {k: v}),
-          '>':  lambda k, v: mk_Q("gt", {k: v}),
-          '>=': lambda k, v: mk_Q("gte", {k: v}),
-          "?=": lambda k, v: existance,
-        }
-
-        return ops[test_str]
+        return lambda k, v: "{0}{1}{2}".format(k, test_str, v)
 
     def mk_cmp(self, key, val, test):
-        return test(key, self.to_n_dig_int(val) )
+        def cond_cast(v):
+            # there should not be non-strings but...
+            v = str(v) if not isinstance(v, str) else v
+            if v.isdigit():
+                return "::integer"
+            else:
+                if '.' in v:
+                    # could be float
+                    try:
+                        f = float(v)
+                        return "::real"
+                    except ValueError:
+                        pass
+            return ""
 
-    def to_n_dig_int(self, num, digits=6):
-        num_str = "%0" + str(digits) + "d"
-        return num_str % int(num) if str(num).isdigit() else num
+        key = "({0}->'{1}'){2}".format(self.field, key, cond_cast(val))
+        return test( key, val )
 
     def call(self, predicate, queryset):
-        return queryset.filter(predicate)
+        return queryset.extra(where=[predicate]) if predicate else queryset
--- test/test_db.py
+++ test/test_db.py
@@ -50,95 +50,63 @@ class SomeModelCase(TestCase):
 
         self.d = NYCSatScores.objects.all()
 
-    def to_n_dig_int(num, digits=6):
-        num_str = "%0" + str(digits) + "d"
-        if isinstance(num, str):
-            return num_str % int(num) if num.isdigit() else num
-        else:
-            return num_str % num
-
     def filter(self, daff_src):
         return Daffodil(daff_src, delegate=HStoreQueryDelegate("hstore_col") )(self.d)
 
     def assert_filter_has_n_results(self, n, daff_src):
         self.assertEqual(len(self.filter(daff_src)), n)
 
-    # def test_no_filters(self):
-    #     self.assertEqual(len(self.d), 421)
-    #
-    # def test_empty(self):
-    #     self.assert_filter_has_n_results(421, "")
-    #
-    # def test_empty2(self):
-    #     self.assert_filter_has_n_results(421, "")
-    #
-    # def test_empty3(self):
-    #     self.assert_filter_has_n_results(421, "{}")
-    #
-    # def test_int_eq(self):
-    #     self.assert_filter_has_n_results(4, """
-    #         num_of_sat_test_takers = 50
-    #     """)
 
-    # def test_int_gt(self):
-    #     self.assert_filter_has_n_results(10, """
-    #         sat_writing_avg_score > 100
-    #     """)
-    #
-    def test_int_eq_one(self):
+    def test_no_filters(self):
+        self.assertEqual(len(self.d), 421)
 
-        import time
-        start = time.time()
-        some_list = NYCSatScores.objects.extra(where=["(hstore_col->'sat_writing_avg_score')::integer >0"])
-        print "*"*40
-        print len( some_list )
-        end = time.time()
-        print end - start
-        print "*"*40
-        b = len( NYCSatScores.objects.filter( hstore_col__gt={'sat_writing_avg_score': '000099' } ))>0
-        # self.assert_filter_has_n_results(1, """
-        #     sat_writing_avg_score = 628
-        # """)
+    def test_empty(self):
+        self.assert_filter_has_n_results(421, "")
+        self.assert_filter_has_n_results(421, "{}")
+        self.assert_filter_has_n_results(0, "[]")
 
-    #
-    # hstore
-    #
-    # def test_basic_hstore(self):
-    #     instance = SomeModel.objects.create(cname='something', hstore_col={'a': '1', 'b': '2'})
-    #     assert instance.hstore_col['a'] == '1'
-    #
-    # def test_basic_hstore_with_filter_1(self):
-    #     instance = SomeModel.objects.create(cname='something_else', hstore_col={'first': '5', 'second': '2'})
-    #     instance.save()
-    #
-    #     assert SomeModel.objects.filter( hstore_col__contains={'first': '5', 'second': '2'} )
-    #
-    # def test_basic_hstore_with_contains_key(self):
-    #     instance = SomeModel.objects.create(cname='something_else', hstore_col={'first': '10', 'second': '20'})
-    #     instance.save()
-    #     assert len( SomeModel.objects.filter( hstore_col__contains='first' ))>0
-    #
-    # def test_basic_hstore_with_contains_value(self):
-    #     instance = SomeModel.objects.create(cname='something_else', hstore_col={'first': '55', 'second': '77'})
-    #     instance.save()
-    #     assert len( SomeModel.objects.filter( hstore_col__contains='55' ))>0
-    #
-    # def test_basic_hstore_with_gt_value(self):
-    #     instance = SomeModel.objects.create(cname='something_else', hstore_col={'first': '55', 'second': '77'})
-    #     instance.save()
-    #     assert len( SomeModel.objects.filter( hstore_col__gt={'first': '54'} ))>0
-    #
-    # def test_basic_hstore_with_get(self):
-    #     instance = SomeModel.objects.create(cname='something_else245', hstore_col={'first': '100', 'second': '200'})
-    #     assert SomeModel.objects.get( cname="something_else245").hstore_col['first'] == '100'
-    #
-    # def test_basic_hstore_with_q(self):
-    #     from django.db.models import Q
-    #     instance = SomeModel.objects.create(cname='something_else245', hstore_col={'first': '100', 'second': '200'})
-    #     instance.save()
-    #     assert SomeModel.objects.filter( Q(hstore_col__gte= {'first':'100'}) | Q(hstore_col__lt= {'second':'100'}) )
+    def test_int_eq(self):
+        self.assert_filter_has_n_results(4, """
+            num_of_sat_test_takers = 50
+        """)
+    def test_int_ne(self):
+        self.assert_filter_has_n_results(417, """
+            num_of_sat_test_takers != 50
+        """)
+
+    def test_int_gt(self):
+        self.assert_filter_has_n_results(273, """
+            num_of_sat_test_takers > 50
+        """)
+
+    def test_int_gte(self):
+        self.assert_filter_has_n_results(277, """
+            num_of_sat_test_takers >= 50
+        """)
+
+    def test_int_lt(self):
+        self.assert_filter_has_n_results(144, """
+            num_of_sat_test_takers < 50
+        """)
+
+    def test_int_lte(self):
+        self.assert_filter_has_n_results(148, """
+            num_of_sat_test_takers <= 50
+        """)
+
+    def test_float_eq(self):
+        self.assert_filter_has_n_results(0, """
+            num_of_sat_test_takers = 50.5
+        """)
 
 if __name__ == "__main__":
+
+    ids = (n.id for n in NYCSatScores.objects.all())
+    for id in ids:
+        n = NYCSatScores.objects.get( pk= id)
+        n.delete()
+        n.save()
+
 
     from django.core.management import call_command
     call_command('syncdb', interactive=False)
