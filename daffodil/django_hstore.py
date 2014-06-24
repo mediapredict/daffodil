7af58890377b94acd7dbc55ca6512eb31f35cc77
James Robert <jiaaro@gmail.com>
2014-06-24 11:03:55 -0400

    use Igor's implementation of django_hstore

--- daffodil/django_hstore.py
+++ daffodil/django_hstore.py
@@ -1,54 +1,54 @@
 
 
 class HStoreQueryDelegate(object):
-    
+
     def __init__(self, hstore_field_name):
         self.field = hstore_field_name
 
     def mk_any(self, children):
-        from django.db.models import Q
-        q = Q()
-        for child_q in children:
-            q |= child_q
-        return q
+        return " OR ".join( "(" + child_exp + ")" for child_exp in children)
 
     def mk_all(self, children):
-        from django.db.models import Q
-        q = Q()
-        for child_q in children:
-            q &= child_q
-        return q
+        if children == ['']:
+            r = "0=1"
+        else:
+            r = " AND ".join( "(" + child_exp + ")" for child_exp in children if child_exp)
+        return r
 
     def mk_test(self, test_str):
-        from django.db.models import Q
-        def mk_Q(cmp, hstore_val, negate=False):
-            k = "{0}__{1}".format(self.field, cmp)
-            q = Q(**{k: hstore_val})
-            return q
-
-        def existance(k, v):
-            q = mk_Q("contains", [k])
-            if not v:
-                q = ~q
-            return q
-
-        def strval(v):
-            return unicode(v)
-
-        ops = {
-          '=':  lambda k, v: mk_Q("contains", {k: strval(v)}),
-          '!=': lambda k, v: ~mk_Q("contains", {k: strval(v)}),
-          '<':  lambda k, v: mk_Q("lt", {k: strval(v)}),
-          '<=': lambda k, v: mk_Q("lte", {k: strval(v)}),
-          '>':  lambda k, v: mk_Q("gt", {k: strval(v)}),
-          '>=': lambda k, v: mk_Q("gte", {k: strval(v)}),
-          "?=": existance,
-        }
-
-        return ops[test_str]
+        if test_str == "?=":
+            existance = lambda k, v: "{0}?{1}".format(k, v)
+            existance.is_datapoint_test = True
+            return existance
+        else:
+            return lambda k, v: "{0}{1}{2}".format(k, test_str, v)
 
     def mk_cmp(self, key, val, test):
-        return test(key, val)
+        if getattr(test, "is_datapoint_test", False):
+            # here we cover:
+            # [NOT] hstore_col ? '1ukmoviestudios - Disney'
+            negate = "NOT " if val == False else ""
+            val = "'{0}'".format(key)
+            key = "{0}{1}".format(negate, self.field)
+        else:
+            cast, val = self.cond_cast(val)
+            key = "({0}->'{1}'){2}".format(self.field, key, cast)
+        return test( key, val )
+
+    def cond_cast(self, v):
+        # should be strings but...
+        v = str(v) if not isinstance(v, str) else v
+        if v.isdigit():
+            return "::integer", v
+        else:
+            if '.' in v:
+                # could be float
+                try:
+                    f = float(v)
+                    return "::real", v
+                except ValueError:
+                    pass
+        return "", "'{0}'".format(v)
 
     def call(self, predicate, queryset):
-        return queryset.filter(predicate)
+        return queryset.extra(where=[predicate]) if predicate else queryset
--- test/test.py
+++ test/test.py
@@ -24,78 +24,78 @@ class SATDataTests(unittest.TestCase):
 
     def filter(self, daff_src):
         return Daffodil(daff_src)(self.d)
-    
+
     def assert_filter_has_n_results(self, n, daff_src):
         self.assertEqual(len(self.filter(daff_src)), n)
 
     def test_no_filters(self):
         self.assertEqual(len(self.d), 421)
-    
+
     def test_empty(self):
         self.assert_filter_has_n_results(421, "")
         self.assert_filter_has_n_results(421, "{}")
         self.assert_filter_has_n_results(0, "[]")
-        
+
     def test_int_eq(self):
         self.assert_filter_has_n_results(4, """
             num_of_sat_test_takers = 50
         """)
-    
+
     def test_int_ne(self):
         self.assert_filter_has_n_results(417, """
             num_of_sat_test_takers != 50
         """)
-        
+
     def test_int_gt(self):
         self.assert_filter_has_n_results(273, """
             num_of_sat_test_takers > 50
         """)
-    
+
     def test_int_gte(self):
         self.assert_filter_has_n_results(277, """
             num_of_sat_test_takers >= 50
         """)
-    
+
     def test_int_lt(self):
         self.assert_filter_has_n_results(144, """
             num_of_sat_test_takers < 50
         """)
-        
+
     def test_int_lte(self):
         self.assert_filter_has_n_results(148, """
             num_of_sat_test_takers <= 50
         """)
-    
+
     def test_float_eq(self):
         self.assert_filter_has_n_results(0, """
             num_of_sat_test_takers = 50.5
         """)
-    
+
     def test_float_ne(self):
         self.assert_filter_has_n_results(421, """
             num_of_sat_test_takers != 50.5
         """)
-        
+
     def test_float_gt(self):
         self.assert_filter_has_n_results(273, """
             num_of_sat_test_takers > 50.5
         """)
-    
+
     def test_float_gte(self):
         self.assert_filter_has_n_results(277, """
             num_of_sat_test_takers >= 50.0
         """)
-    
+
     def test_float_lt(self):
         self.assert_filter_has_n_results(144, """
             num_of_sat_test_takers < 49.5
         """)
-        
+
     def test_float_lte(self):
         self.assert_filter_has_n_results(148, """
             num_of_sat_test_takers <= 50.0
         """)
-    
+
     def test_multiple(self):
         self.assert_filter_has_n_results(3, """
             num_of_sat_test_takers = 10
@@ -103,13 +103,13 @@ class SATDataTests(unittest.TestCase):
             sat_math_avg_score > 200
             sat_critical_reading_avg_score <= 500
         """)
-    
+
     def test_multiple_for_same_value(self):
         self.assert_filter_has_n_results(51, """
             sat_writing_avg_score >= 300
             sat_writing_avg_score < 350
         """)
-    
+
     def test_or(self):
         self.assert_filter_has_n_results(8, """
             [
@@ -118,7 +118,7 @@ class SATDataTests(unittest.TestCase):
                 num_of_sat_test_takers = 12
             ]
         """)
-    
+
     def test_and_nested_within_or(self):
         self.assert_filter_has_n_results(134, """
             [
@@ -132,7 +132,7 @@ class SATDataTests(unittest.TestCase):
                 }
             ]
         """)
-    
+
     def test_or_mixed_with_literal(self):
         self.assert_filter_has_n_results(11, """
             sat_writing_avg_score < 450
@@ -145,7 +145,7 @@ class SATDataTests(unittest.TestCase):
                 num_of_sat_test_takers = 15
             ]
         """)
-    
+
     def test_and_mixed_with_or(self):
         self.assert_filter_has_n_results(6, """
             {
@@ -158,7 +158,7 @@ class SATDataTests(unittest.TestCase):
                 num_of_sat_test_takers = 12
             ]
         """)
-    
+
     def test_single_quoted_fields(self):
         self.assert_filter_has_n_results(417, """
             'num_of_sat_test_takers' != 50
@@ -166,7 +166,7 @@ class SATDataTests(unittest.TestCase):
         self.assert_filter_has_n_results(417, """
             'num_of_sat_test_takers' != '50'
         """)
-    
+
     def test_double_quoted_fields(self):
         self.assert_filter_has_n_results(417, """
             "num_of_sat_test_takers" != 50
@@ -174,7 +174,7 @@ class SATDataTests(unittest.TestCase):
         self.assert_filter_has_n_results(417, """
             "num_of_sat_test_takers" != "50"
         """)
-    
+
     def test_string_eq(self):
         self.assert_filter_has_n_results(1, """
             school_name = "EAST SIDE COMMUNITY SCHOOL"
@@ -182,7 +182,7 @@ class SATDataTests(unittest.TestCase):
         self.assert_filter_has_n_results(1, """
             school_name = 'EAST SIDE COMMUNITY SCHOOL'
         """)
-    
+
     def test_string_ne(self):
         self.assert_filter_has_n_results(420, """
             school_name != "EAST SIDE COMMUNITY SCHOOL"
@@ -190,17 +190,17 @@ class SATDataTests(unittest.TestCase):
         self.assert_filter_has_n_results(420, """
             school_name != 'EAST SIDE COMMUNITY SCHOOL'
         """)
-    
+
     def test_comparing_a_string_containing_int(self):
         self.assert_filter_has_n_results(417, """
             num_of_sat_test_takers != "50"
         """)
-    
+
     def test_comparing_a_string_containing_float(self):
         self.assert_filter_has_n_results(417, """
             num_of_sat_test_takers != "50.0"
         """)
-    
+
     def test_missing_field(self):
         self.assert_filter_has_n_results(0, """
             numOfSatTestTakers-9-17-2013 = 99
@@ -214,17 +214,17 @@ class SATDataTests(unittest.TestCase):
         self.assert_filter_has_n_results(421, """
             "number of SAT Test Takers 9-17-2013" != 99
         """)
-    
+
     def test_invalid_filter(self):
         self.assertRaises(ParseError, Daffodil, """
             [
         """)
-        
+
     def test_unicode_filter(self):
         self.assert_filter_has_n_results(273, u"""
             num_of_sat_test_takers > 50
         """)
-    
+
     def test_existance_has_value(self):
         self.assert_filter_has_n_results(421, u"""
             num_of_sat_test_takers ?= true
@@ -244,7 +244,7 @@ class SATDataTests(unittest.TestCase):
         self.assert_filter_has_n_results(0, u"""
             'num_of_sat_test_takers' ?= false
         """)
-    
+
     def test_existance_does_not_have_value(self):
         self.assert_filter_has_n_results(0, u"""
             asdf ?= true
@@ -264,29 +264,29 @@ class SATDataTests(unittest.TestCase):
         self.assert_filter_has_n_results(421, u"""
             'asdf' ?= false
         """)
-        
+
 
 class PredicateTests(unittest.TestCase):
     def setUp(self):
         self.data = {
-          "MHQ9ProgsWatched - GoldRush": "no", 
-          "MHQ9ProgsWatched - PawnStars": "yes", 
-          "MHQ9ProgsWatched - BornSurvivor": "no", 
-          "MHQ9ProgsWatched - AirCrashInvestigation": "no", 
-          "MHQ9ProgsWatched - AmericanPickers": "no", 
-          "MHQ9ProgsWatched - StorageWars": "yes", 
-          "MHQ9ProgsWatched - TopGear": "no", 
-          "MHQ9ProgsWatched - FifthGear": "no", 
-          "MHQ9ProgsWatched - WheelerDealers": "no", 
-          "MHQ9ProgsWatched - FastNLoud": "no", 
-          "MHQ9ProgsWatched - MythBusters": "no", 
-          "MHQ9ProgsWatched - DeadliestCatch": "no", 
-          "MHQ9ProgsWatched - WickedTuna": "no", 
-          "MHQ9ProgsWatched - AuctionHunters": "no", 
-          "MHQ9ProgsWatched - AmericanChopper": "no", 
+          "MHQ9ProgsWatched - GoldRush": "no",
+          "MHQ9ProgsWatched - PawnStars": "yes",
+          "MHQ9ProgsWatched - BornSurvivor": "no",
+          "MHQ9ProgsWatched - AirCrashInvestigation": "no",
+          "MHQ9ProgsWatched - AmericanPickers": "no",
+          "MHQ9ProgsWatched - StorageWars": "yes",
+          "MHQ9ProgsWatched - TopGear": "no",
+          "MHQ9ProgsWatched - FifthGear": "no",
+          "MHQ9ProgsWatched - WheelerDealers": "no",
+          "MHQ9ProgsWatched - FastNLoud": "no",
+          "MHQ9ProgsWatched - MythBusters": "no",
+          "MHQ9ProgsWatched - DeadliestCatch": "no",
+          "MHQ9ProgsWatched - WickedTuna": "no",
+          "MHQ9ProgsWatched - AuctionHunters": "no",
+          "MHQ9ProgsWatched - AmericanChopper": "no",
           "MHQ9ProgsWatched - SonsOfGuns": "no"
         }
-    
+
     def test_matching(self):
         daff = Daffodil("""
         {
@@ -303,7 +303,7 @@ class PredicateTests(unittest.TestCase):
         }
         """)
         self.assertTrue(daff.predicate(self.data))
-        
+
         daff = Daffodil(u"""{
            "MHQ9ProgsWatched - BornSurvivor" = 'no'
            "MHQ9ProgsWatched - WheelerDealers" = 'no'
@@ -317,9 +317,9 @@ class PredicateTests(unittest.TestCase):
            "MHQ9ProgsWatched - AmericanChopper" = 'no'
         }""")
         self.assertTrue(daff.predicate({
-           u'MHQ9ProgsWatched - PawnStars': u'yes', u'MHQ3Gender': u'Female', u'MHQ9ProgsWatched - FastNLoud': u'no', u'MHQ8ChannelViewing - Discovery Channel': u'OncePerWeek', u'MHQ8ChannelViewing - National Geographic': u'OncePerWeek', '_sample_id': u'frutest3', u'MHQ4TVProvider - YouView': u'no', u'MHQ6Industry - MarketResearch': u'no', u'MHQ6Industry - Entertainment': u'no', u'MHQ5TVViewing': u'1-3nights', u'MHQ8ChannelViewing - ITV1': u'OncePerWeek', u'MHQ7PastParticipant': u'No', u'MHQ9ProgsWatched - StorageWars': u'yes', u'MHQ8ChannelViewing - History Channel': u'OncePerWeek', u'MHQ8ChannelViewing - BBC2': u'OncePerWeek', u'MHQ1Confidential': u'Yes', u'MHQ4TVProvider - Freeview': u'no', u'MHQ9ProgsWatched - BornSurvivor': u'no', u'MHQ9ProgsWatched - WheelerDealers': u'no', u'MHQ8ChannelViewing - Channel4': u'OncePerWeek', u'MHQ4TVProvider - IPTV': u'no', u'MHQ9ProgsWatched - AuctionHunters': u'no', u'MHQ9ProgsWatched - AirCrashInvestigation': u'no', u'MHQ8ChannelViewing - Sky Arts': u'OncePerWeek', u'MHQ2Age': u'18-34', u'MHQ8ChannelViewing - BBC1': u'OncePerWeek', u'MHQ6Industry - None': u'yes', u'MHQ4TVProvider - None': u'no', u'MHQ6Industry - RadioTV': u'no', u'MHQ6Industry - Advertising': u'no', u'MHQ9ProgsWatched - DeadliestCatch': u'no', u'MHQ9ProgsWatched - FifthGear': u'no', u'MHQ9ProgsWatched - TopGear': u'no', u'MHQ9ProgsWatched - SonsOfGuns': u'no', u'MHQ9ProgsWatched - WickedTuna': u'no', u'MHQ9ProgsWatched - GoldRush': u'no', u'MHQ9ProgsWatched - MythBusters': u'no', u'MHQ6Industry - Press': u'no', u'MHQ4TVProvider - Virgin': u'yes', '_id': '41298', u'MHQ9ProgsWatched - AmericanPickers': u'no', u'MHQ9ProgsWatched - AmericanChopper': u'no', u'MHQ4TVProvider - Sky': u'no'                             
+           u'MHQ9ProgsWatched - PawnStars': u'yes', u'MHQ3Gender': u'Female', u'MHQ9ProgsWatched - FastNLoud': u'no', u'MHQ8ChannelViewing - Discovery Channel': u'OncePerWeek', u'MHQ8ChannelViewing - National Geographic': u'OncePerWeek', '_sample_id': u'frutest3', u'MHQ4TVProvider - YouView': u'no', u'MHQ6Industry - MarketResearch': u'no', u'MHQ6Industry - Entertainment': u'no', u'MHQ5TVViewing': u'1-3nights', u'MHQ8ChannelViewing - ITV1': u'OncePerWeek', u'MHQ7PastParticipant': u'No', u'MHQ9ProgsWatched - StorageWars': u'yes', u'MHQ8ChannelViewing - History Channel': u'OncePerWeek', u'MHQ8ChannelViewing - BBC2': u'OncePerWeek', u'MHQ1Confidential': u'Yes', u'MHQ4TVProvider - Freeview': u'no', u'MHQ9ProgsWatched - BornSurvivor': u'no', u'MHQ9ProgsWatched - WheelerDealers': u'no', u'MHQ8ChannelViewing - Channel4': u'OncePerWeek', u'MHQ4TVProvider - IPTV': u'no', u'MHQ9ProgsWatched - AuctionHunters': u'no', u'MHQ9ProgsWatched - AirCrashInvestigation': u'no', u'MHQ8ChannelViewing - Sky Arts': u'OncePerWeek', u'MHQ2Age': u'18-34', u'MHQ8ChannelViewing - BBC1': u'OncePerWeek', u'MHQ6Industry - None': u'yes', u'MHQ4TVProvider - None': u'no', u'MHQ6Industry - RadioTV': u'no', u'MHQ6Industry - Advertising': u'no', u'MHQ9ProgsWatched - DeadliestCatch': u'no', u'MHQ9ProgsWatched - FifthGear': u'no', u'MHQ9ProgsWatched - TopGear': u'no', u'MHQ9ProgsWatched - SonsOfGuns': u'no', u'MHQ9ProgsWatched - WickedTuna': u'no', u'MHQ9ProgsWatched - GoldRush': u'no', u'MHQ9ProgsWatched - MythBusters': u'no', u'MHQ6Industry - Press': u'no', u'MHQ4TVProvider - Virgin': u'yes', '_id': '41298', u'MHQ9ProgsWatched - AmericanPickers': u'no', u'MHQ9ProgsWatched - AmericanChopper': u'no', u'MHQ4TVProvider - Sky': u'no'
         }))
-    
+
     def test_not_matching(self):
         daff = Daffodil("""
         {
@@ -335,20 +335,20 @@ class PredicateTests(unittest.TestCase):
            "MHQ9ProgsWatched - AmericanChopper" = "no"
         }
         """)
-        
+
         self.assertFalse(daff.predicate(self.data))
 
 
 
 # Borrowed gratuitously from https://gist.github.com/k4ml/2219751
 from os import path as osp
- 
+
 def rel_path(*p):
     return osp.normpath(osp.join(rel_path.path, *p))
 
 rel_path.path = osp.abspath(osp.dirname(__file__))
 this = osp.splitext(osp.basename(__file__))[0]
- 
+
 from django.conf import settings
 SETTINGS = dict(
     SITE_ID=1,
@@ -365,7 +365,7 @@ SETTINGS = dict(
     ],
     ROOT_URLCONF=this,
 )
- 
+
 if not settings.configured:
     settings.configure(**SETTINGS)
 
@@ -394,7 +394,7 @@ class SATDataTestsWithHStore(SATDataTests):
         daff = Daffodil(daff_src, delegate=delegate)
         return daff(self.d)
 
-        
+
 
 get_app_orig = models.get_app
 def get_app(app_label,*a, **kw):
@@ -408,11 +408,15 @@ models.loading.cache.app_store[type(this+'.models',(),{'__file__':__file__})] = 
 from django.core import management
 
 if __name__ == "__main__":
+    from django.db import connection
+    cursor = connection.cursor()
+    cursor.execute("create extension if not exists hstore")
+
     management.call_command("syncdb")
-    
+
     BasicHStoreData.objects.all().delete()
     for record in load_test_data('nyc_sat_scores'):
         BasicHStoreData.objects.create(hsdata=record)
 
     unittest.main()
-    
+
