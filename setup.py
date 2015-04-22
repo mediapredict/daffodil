4a36283ac196f5c5832d32ba656aca7c651183f4
James Robert <github@jiaaro.com>
2015-04-22 10:27:46 -0400

    Increment version with bug fixes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='daffodil',
-    version='0.3.0',
+    version='0.3.1',
     author='James Robert',
     description='A Super-simple DSL for filtering datasets',
     license='MIT',
