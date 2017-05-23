d47aaab118bfdac202dbe98a63c792c5c74d6bce
James Robert <me@jiaaro.com>
2017-05-23 17:05:29 -0400

    increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='daffodil',
-    version='0.3.15',
+    version='0.3.16',
     author='James Robert',
     description='A Super-simple DSL for filtering datasets',
     license='MIT',
