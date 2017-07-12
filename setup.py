8816ca0f592c27a68854ff7b74138e4895b1c99e
James Robert <github@jiaaro.com>
2017-07-12 12:43:08 -0400

    increment version for SimulationMatchingDelegate

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='daffodil',
-    version='0.3.18',
+    version='0.3.19',
     author='James Robert',
     description='A Super-simple DSL for filtering datasets',
     license='MIT',
