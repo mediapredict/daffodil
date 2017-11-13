3722ca54749b60fc47fb1e8c6b1de66f4c5ed9a9
James Robert <github@jiaaro.com>
2017-11-13 15:31:38 -0500

    Fix for python 3-specific type coercion bug

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='daffodil',
-    version='0.4.0',
+    version='0.4.1',
     author='James Robert',
     description='A Super-simple DSL for filtering datasets',
     license='MIT',
