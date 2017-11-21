25105a5370d1f325af522084b5849801df7db8db
James Robert <me@jiaaro.com>
2017-11-21 12:14:24 -0500

    found a way to make install work without first installing cython

--- setup.py
+++ setup.py
@@ -1,22 +1,15 @@
 from distutils.core import setup
 
-# TODO: in the future distribute c sources instead of pyx files
-#   https://stackoverflow.com/questions/4505747/how-should-i-structure-a-python-package-that-contains-cython-code
-from Cython.Build import cythonize
-import Cython.Compiler.Options
-Cython.Compiler.Options.cimport_from_pyx = True
+extra_kwargs = {}
 
+try:
+    # TODO: in the future distribute c sources instead of pyx files
+    #   https://stackoverflow.com/questions/4505747/how-should-i-structure-a-python-package-that-contains-cython-code
+    from Cython.Build import cythonize
+    import Cython.Compiler.Options
+    Cython.Compiler.Options.cimport_from_pyx = True
 
-setup(
-    name='daffodil',
-    version='0.5.1',
-    author='James Robert',
-    description='A Super-simple DSL for filtering datasets',
-    license='MIT',
-    keywords='data filtering',
-    url='https://github.com/mediapredict/daffodil',
-    packages=['daffodil'],
-        ext_modules=cythonize(
+    extra_kwargs['ext_modules'] = cythonize(
         'daffodil/*.pyx',
         compiler_directives={
             'language_level': "3",
@@ -25,7 +18,21 @@ setup(
             # has a small performance penalty
             'profile': False,
         }
-    ),
+    )
+except ImportError:
+    pass
+
+
+setup(
+    name='daffodil',
+    version='0.5.2',
+    author='James Robert',
+    description='A Super-simple DSL for filtering datasets',
+    license='MIT',
+    keywords='data filtering',
+    url='https://github.com/mediapredict/daffodil',
+    packages=['daffodil'],
+    install_requires=['cython'],
     long_description='A Super-simple DSL for filtering datasets',
     classifiers=[
         'License :: OSI Approved :: MIT License',
@@ -33,5 +40,6 @@ setup(
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Topic :: Utilities'
-    ]
+    ],
+    **extra_kwargs
 )
