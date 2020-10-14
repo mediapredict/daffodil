081239a20875c197a56d496e0db94d037284ea3b
igor <igor.kramaric@gmail.com>
2020-10-14 19:47:42 +0200

    bump the ver. up

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ if cython_installed:
 
 setup(
     name='daffodil',
-    version='0.5.11',
+    version='0.5.12',
     author='James Robert',
     description='A Super-simple DSL for filtering datasets',
     license='MIT',
