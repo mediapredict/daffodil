c62ff430cdbcf211582eeebfa9eca5f4bf7bd495
igor <igor.kramaric@gmail.com>
2021-06-01 13:08:04 +0200

    bump up ver.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ extra_kwargs = {
 
 setup(
     name='daffodil',
-    version='0.5.13',
+    version='0.5.14',
     author='James Robert',
     description='A Super-simple DSL for filtering datasets',
     license='MIT',
